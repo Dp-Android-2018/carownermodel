f91eebe3232b00168036cf2aebf80e036148b865
m.magdy4112@gmail.com <mohamedmagdy261994>
2018-04-28 10:28:23 +0200

    First Commit

---
+++ app/src/main/java/com/example/mohamedatef/findandfix/Main2Activity.java
@@ -0,0 +1,13 @@
+package com.example.mohamedatef.findandfix;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dialog_add_request_3);
+    }
+}
